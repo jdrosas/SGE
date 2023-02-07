4de5b4ef8cc206480120c070dd51bf09f85d78db
jdrosas <juandiazjdrd@gmail.com>
2023-02-07 00:43:47 -0500

    Agregando Ruteos

--- FE-SGE/src/app/app-routing.module.ts
+++ FE-SGE/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Componentes
+
+import { AgregarEditarAsignacionesComponent } from './components/agregar-editar-asignaciones/agregar-editar-asignaciones.component';
+import { ListadoAsignacionesComponent } from './components/listado-asignaciones/listado-asignaciones.component';
+import { VerAsignacionesComponent } from './components/ver-asignaciones/ver-asignaciones.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'ListadoAsignaciones', pathMatch: 'full'},
+  { path: 'ListadoAsignaciones', component: ListadoAsignacionesComponent},
+  { path: 'AgregarAsignaciones', component: AgregarEditarAsignacionesComponent},
+  { path: 'VerAsignaciones', component: VerAsignacionesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
