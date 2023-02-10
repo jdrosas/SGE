fa361b7cd1e06b39728c5614c88a3ff75d275063
jdrosas <juandiazjdrd@gmail.com>
2023-02-09 22:14:20 -0500

    Agregando controlador Equipo y endpoint

---
+++ BE-SGE/BE-SGE/Controllers/EquipoController.cs
@@ -0,0 +1,34 @@
+﻿using BE_SGE.DataAccess;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BE_SGE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EquipoController : ControllerBase
+    {
+        private readonly SGEContext _context;
+
+        public EquipoController(SGEContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var listEquipo = await _context.Equipos.ToListAsync();
+                return Ok(listEquipo);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            
+        }
+    }
+}
--- BE-SGE/BE-SGE/Program.cs
+++ BE-SGE/BE-SGE/Program.cs
@@ -1,3 +1,6 @@
+using BE_SGE.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Añadiendo Contexto
+builder.Services.AddDbContext<SGEContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SGEConexion"));
+});
+
 
 var app = builder.Build();
 
